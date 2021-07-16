efde714eb8cade07144bd9084dba7dfbf60cf235
Luke-Siganto <84109974+Luke-Siganto@users.noreply.github.com>
2021-07-16 13:47:05 +1000

    mobile home
    mobile index finished

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function openmenu() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
+      hamburger.style.backgroundColor = "#FFFFFF";
+      hamcol.style.backgroundColor = "#FFFFFF";
+      hamburger.style.color = "#379D8A";
     } else {
       x.style.display = "block";
+      hamburger.style.backgroundColor = "#379D8A";
+      hamcol.style.backgroundColor = "#379D8A";
+      hamburger.style.color = "#FFFFFF";
     }
   }
